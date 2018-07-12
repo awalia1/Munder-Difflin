e063664a3dc84c7822d25573eeaaa6e8f5a3d30c
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-07-12 13:46:02 -0400

    added js and buttons

---
+++ main.js
@@ -0,0 +1,13 @@
+// var sections = document.getElementsByTagName('section');
+
+// window.onscroll = function() {
+//     for(i = 0; i < sections.length; i++){
+//         if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+//             sections[i].style.display="block";
+//         }
+//     }
+// };
+
+function buy(){
+	alert("you bought something");
+}
